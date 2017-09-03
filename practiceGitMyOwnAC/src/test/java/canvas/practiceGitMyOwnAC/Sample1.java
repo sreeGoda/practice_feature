16e96e5cfc32f9c380581eceff51229311639ad3
skiranmai <skiranmai78@gmail.com>
2017-09-03 00:03:44 -0700

    test

--- practiceGitMyOwnAC/src/test/java/canvas/practiceGitMyOwnAC/Sample1.java
+++ practiceGitMyOwnAC/src/test/java/canvas/practiceGitMyOwnAC/Sample1.java
@@ -5,6 +5,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		System.out.println("first line.....");
 		System.out.println("second line....");
+		System.out.println("nklkmnkk");
 	}
 
 }
